b432cedfd103bab87c37f175eb5ffc701adf71ce
jnferfer <jnferfer@gmail.com>
2024-01-08 17:59:40 +0100

    add comments and docstring types

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+"""
+Code for running the TextMagnet web app (Version 1.0), based on Streamlit. You can find documentation about
+the application at 'https://textmagnet.streamlit.app/' (About section)
+
+The NLP engine is proprietary, this app merely visualizes the results of the NLP module using data from the
+ MedQuAD dataset (https://paperswithcode.com/dataset/medquad).
+
+Author: Juan Fernández
+"""
+
+from about import *
 from ast import literal_eval
 from styles import *
 from st_click_detector import click_detector
 from streamlit_agraph import agraph, Node, Edge, Config
 from streamlit_gsheets import GSheetsConnection
-from about import *
+import pandas as pd
 import time
 
 
 @st.cache_data(show_spinner=False, ttl="7m")
-def load_data_and_titles():
+def load_data_and_titles() -> tuple:
     """
     Read data from GSheets.
 
     :return: Tuple with dataframe and a list of titles
+    :rtype: tuple
     """
 
     conn = st.connection("gsheets", type=GSheetsConnection)
@@ -24,14 +36,18 @@ def load_data_and_titles():
     return data, titles
 
 
-def build_text(data, doc_id, clicked_sent_id):
+def build_text(data: pd.DataFrame, doc_id: str, clicked_sent_id: str) -> str:
     """
     Generate the text sentence by sentence, and apply the appropriate styles to it.
 
     :param data: data source to fetch sentences from
+    :type data: pd.DataFrame
     :param doc_id: id of the doc to fetch sentences from
+    :type doc_id: str
     :param clicked_sent_id: if True, highlight this sentence
+    :type clicked_sent_id: str
     :return: the generated text string
+    :rtype: str
     """
 
     if clicked_sent_id:
@@ -48,7 +64,7 @@ def build_text(data, doc_id, clicked_sent_id):
         # Add a line break if sent. belongs to a dotted list
         if sent.startswith("- "):
             sent = f"<br>{sent}"
-            if i+1 < len(zipped) and not zipped[i+1][1].startswith("- "):
+            if i + 1 < len(zipped) and not zipped[i + 1][1].startswith("- "):
                 sent += "<br>"  # line break to the end if last element from dotted list
 
         # Add style of sent. to highlight
@@ -69,23 +85,29 @@ def build_text(data, doc_id, clicked_sent_id):
     return text
 
 
-def build_goal_text(data, doc_id, link_sent_id, color, node_label):
+def build_goal_text(data: pd.DataFrame, doc_id: str, link_sent_id: int, color: str, node_label: str) -> str:
     """
     Generate the goal text excerpt, and apply the appropriate styles to it.
 
     :param data: data source to fetch sentences from
+    :type data: pd.DataFrame
     :param doc_id: id of the doc to fetch sentences from
+    :type doc_id: str
     :param link_sent_id: id of the sent to highlight
+    :type link_sent_id: int
     :param color: color code of the dot
+    :type color: str
     :param node_label: name of the node label
+    :type node_label: str
     :return: the generated goal text excerpt
+    :rtype: str
     """
 
     filtered = data[data["doc_id"] == doc_id]
 
     # Only an excerpt from doc is show (a context of 2 preceding sents.)
-    start_sent_id = link_sent_id-2 if link_sent_id-2 >= 0 else 0
-    filtered = filtered.iloc[start_sent_id:link_sent_id+1]
+    start_sent_id = link_sent_id - 2 if link_sent_id - 2 >= 0 else 0
+    filtered = filtered.iloc[start_sent_id:link_sent_id + 1]
 
     title = filtered["title"].iloc[0]
 
@@ -104,13 +126,16 @@ def build_goal_text(data, doc_id, link_sent_id, color, node_label):
     return goal_text
 
 
-def add_line_breaks(text, words_num_per_line=4):
+def add_line_breaks(text: str, words_num_per_line: int = 4) -> str:
     """
     Add line breaks if text is longer than 4 words.
 
     :param text: Text to edit
+    :type text: str
     :param words_num_per_line: Max. num of words per line
+    :type words_num_per_line: int
     :return: Edited text
+    :rtype: str
     """
 
     words = text.split()
@@ -127,13 +152,15 @@ class Graph:
         self.data = data
         self.config = Config(from_json="graph_config.json")
 
-    def build(self, doc_id_sent_id):
+    def build(self, doc_id_sent_id: str) -> str:
         """
         Build the links' graph using the 'doc_id_sent_id' variable (id of the doc and id of the sent
         to fetch links from).
 
         :param doc_id_sent_id: id of the doc and id of the sent to fetch links from
+        :type doc_id_sent_id: str
         :return: id of the clicked node
+        :rtype: str
         """
 
         nodes = []
@@ -166,7 +193,7 @@ class Graph:
                     Node(
                         id=relation,
                         label=NODE["relation_label"][relation],
-                        title=round(sum(dists)/len(dists), 2),
+                        title=round(sum(dists) / len(dists), 2),
                         size=NODE["size"],
                         color=NODE["color"]["intermediate"][relation]
                     )
@@ -214,9 +241,14 @@ class Graph:
         return selected_link
 
 
-def add_to_history(title, max_length=10):
+def add_to_history(title: str, max_length=10):
     """
-    Append a selected title to history log
+    Append a selected title to history log.
+
+    :param title: The title to append to history log.
+    :type title: str
+    :param max_length: Max. length of history log's list
+    :type max_length: int
     """
 
     st.session_state["history"].append(title)
@@ -366,7 +398,8 @@ if __name__ == "__main__":
                         link_doc_id, link_sent_id, color, node_label = graph_output.split("|")
                         goal_output = click_detector(
                             html_content=build_goal_text(data=data, doc_id=link_doc_id,
-                                                         link_sent_id=int(link_sent_id), color=color, node_label=node_label),
+                                                         link_sent_id=int(link_sent_id), color=color,
+                                                         node_label=node_label),
                             key="clicked_goal"
                         )
 
